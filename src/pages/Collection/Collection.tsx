d216dc8eac7b9121106bba9c0b460d2ddc819263
Michaël Bourjac <michael.bourjac@gmail.com>
2024-10-05 19:24:28 +0200

    feat: display collection base info

--- src/pages/Collection/Collection.tsx
+++ src/pages/Collection/Collection.tsx
@@ -9,7 +9,7 @@ export const Collection = () => {
   const loaderData = useLoaderData({
     from: '/_layout/collections/$collectionId',
   });
-  const { config, baseInfo, pictures } = loaderData.collection;
+  const { config, pictures } = loaderData.collection;
 
   const gridRef = useRef<HTMLDivElement>(null);
   const [loadedPicturesCount, setLoadedPicturesCount] = useState(0);
@@ -30,8 +30,12 @@ export const Collection = () => {
     <main className="col-span-3 col-start-1 row-span-2 row-start-1 grid grid-cols-subgrid grid-rows-subgrid overflow-hidden">
       <div className="relative z-10 col-start-1 row-start-2 whitespace-nowrap px-2 pb-2">
         <div className="flex flex-col gap-2 bg-off-black pt-2">
-          <h2 className="active-marker w-fit bg-white">{baseInfo}</h2>
           <CollectionNav currentCollectionId={config.id} />
+          <div className="flex w-fit flex-col gap-[0.175rem]">
+            <h2 className="bg-white">{`collection ${String(config.id)}`}</h2>
+            <p className="w-fit bg-white">{config.title}</p>
+            <p className="w-fit bg-white">{config.date}</p>
+          </div>
         </div>
       </div>
       <div className="col-start-2 row-span-2 flex flex-col gap-2 overflow-auto p-2">
